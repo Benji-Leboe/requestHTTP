75bf45907e1d8e5cc29fadd79e93a4ed49a1be9b
Benji-Leboe <benjamin.r.leboe@gmail.com>
2018-09-03 18:10:34 +0000

    commit getAndPrintHTMLChunks function

--- get_printChunks.js
+++ get_printChunks.js
@@ -1,9 +1,29 @@
 const https = require('https');
 const fs = require('fs');
 
-const requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step1.html'
-};
 
 
+function getAndPrintHTMLChunks(){
+
+  const requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+  
+  https.get(requestOptions, (response) => {
+    response.setEncoding('utf8');
+  
+    response.on('data', (data) => {
+      console.log(`Chunk length received: ${data.length}`);
+      console.log(`Chunk received: ${data}`)
+    });
+  
+    response.on('end', () => {
+      console.log('Response stream ended.');
+    });
+  
+  });
+}
+
+
+console.log(getAndPrintHTMLChunks())
